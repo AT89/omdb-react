fb04a8555810e687078b1a87aaacdb052e804dde
Anh Truong <anh.truong.07@cnu.edu>
2016-08-03 12:12:51 -0400

    search container added

--- src/Search.js
+++ src/Search.js
@@ -2,30 +2,14 @@ import React, {Component} from "react"
 
 
 class Search extends Component {
-  constructor(props){
-   super(props)
-   this.state = {
-     query: ''
-   }
-  }
-  handleSearchInput (e) {
-   this.setState({
-     query: e.target.value
-   })
-  }
-    submitQuery(evt){
-      evt.preventDefault();
-      console.log(this.state.query)
-      this.setState({query: ''})
-    }
-
-    render(){
+  render(){
+    let {handleSearchInput, handleSubmitQuery, query} = this.props
       return  (
         <div className="searchBox">
-          <form onSubmit={(evt) => this.submitQuery(evt)}>
+          <form onSubmit={(evt) => handlesubmitQuery(evt)}>
             <input
-            onChange={(evt) => this.handleSearchInput(evt)}
-            value={this.state.query}
+            onChange={(evt) => handleSearchInput(evt)}
+            value={query}
             type="text"
             placeholder="Enter a Movie Title..." />
             <button type="submit">Search</button>
